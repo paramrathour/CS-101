ab5af0f76e5ca9e9822a0547207bb20dc2f6ac2b
Param Rathour <rathourparam3435@gmail.com>
2022-06-13 00:50:36 +0530

    Update
    Added Minesweeper

---
+++ Starter Codes/Minesweeper.cpp
@@ -0,0 +1,10 @@
+// Author: param3435
+#include<simplecpp>
+
+const int MAX_n = 50;
+
+main_program{
+	int rows, columns, count_mines, x, y;
+	cin >> rows >> columns >> count_mines;
+	// take input, calculate the output
+}
