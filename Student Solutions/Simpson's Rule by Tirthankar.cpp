3b81eacef3ae9960ee78309cdfa6874cc0bf8eb3
Param Rathour <rathourparam3435@gmail.com>
2022-11-07 16:19:15 +0530

    Update Simpson's Rule by Tirthankar.cpp

--- Student Solutions/Simpson's Rule by Tirthankar.cpp
+++ Student Solutions/Simpson's Rule by Tirthankar.cpp
@@ -4,39 +4,44 @@
 
 using namespace std;
 
-//Problem 3 from the Extra Problems PDF
+//Problem 2.10 from the Extra Problems PDF
 //Link: https://paramrathour.github.io/CS-101/Problems.pdf
 
 long double f(long double x);
 
 int main() {
-	unsigned int n = 34; //Number of points to take
+	int total_test_cases;
+	cin >> total_test_cases;
+	for (int test_cases = 0; test_cases < total_test_cases; test_cases++) {
+		unsigned int n; //Number of points to take
+		cin >> n;
+		long double a, b; //a = lower bound, b = upper bound
+		a = 0;
+		b = 1;
+		n -= 1;
 
-	long double a, b; //a = lower bound, b = upper bound
-	a = 0.5;
-	b = 1;
+		long double step = (b - a) / n; //Stores the step size, which
+		//depends on the value of n
 
-	long double step = (b - a) / n; //Stores the step size, which
-	//depends on the value of n
+		long double ans = 0;
 
-	long double ans = 0;
-
-	for (int i = 0; i <= n; i++) {
-		long double x = a + i * step;
-		if (i == 0 || i == n) {
-			ans += f(x);
-		} else if (i % 2 == 0) {
-			ans += 2 * f(x);
-		} else {
-			ans += 4 * f(x);
+		for (int i = 0; i <= n; i++) {
+			long double x = a + i * step;
+			if (i == 0 || i == n) {
+				ans += f(x);
+			} else if (i % 2 == 0) {
+				ans += 2 * f(x);
+			} else {
+				ans += 4 * f(x);
+			}
 		}
+
+		ans *= step / 3;
+
+		cout << fixed << setprecision(15) << (long double) 22 / 7 - ans << endl;
 	}
-
-	ans *= step / 3;
-
-	cout << fixed << setprecision(8) << ans << endl;
 }
 
 long double f(long double x) {
-	return sin(x) / x;
+	return pow(x, 4) * pow(1 - x, 4) / (1 + pow(x, 2));
 }
