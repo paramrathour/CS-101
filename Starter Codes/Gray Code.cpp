c5a3b298f8e54e49a91e3c23560624a05a95a32c
Param Rathour <rathourparam3435@gmail.com>
2023-03-02 00:14:55 +0530

    Update
    Added Gray Code
    More Clarity for some section titles
    Fixed typos

---
+++ Starter Codes/Gray Code.cpp
@@ -0,0 +1,15 @@
+#include<simplecpp>
+
+const int MAX_n = 10;
+const int MAX_size = 1024;
+
+main_program {
+	int n, power = 2;
+	cin >> n;
+	int size = 1;
+	repeat(n) {
+		size *= 2;
+	}
+	int digits[MAX_size][MAX_n];
+	// generate gray code here
+}
